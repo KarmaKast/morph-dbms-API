954b2037415c1fc9f1464adfe5e1e7731236f7ef
sree chandan <sreechandan98@gmail.com>
2020-04-12 04:06:54 +0530

    improvements

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,50 @@
 import express from "express";
 import { Request, Response } from "express";
+import * as morphCore from "@karmakast/morph-dbms-core";
+import * as morphViz from "@karmakast/morph-dbms-viz";
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
 import { config } from "./config";
 app.use("/config", config);
+const configTest = {
+  databasePath: "./data",
+};
 
 app.all("/", function (_request: Request, response: Response) {
   response.send("Morph DBMS Server");
+});
+
+let vizSession: morphViz.Viz;
+app.post("/collection/create", function (req, res) {
+  const label = req.body.label;
+  console.log("label : ", label);
+
+  vizSession = new morphViz.Viz(label);
+  res.send("Success");
+});
+
+app.post("/collection/save", function (req, res) {
+  vizSession.save(configTest.databasePath);
+  res.send("Success");
+});
+
+app.post("/collection/load", function (req, res) {
+  vizSession = morphViz.load(
+    configTest.databasePath,
+    req.body.collectionID,
+    req.body.label
+  );
+  res.send("Success");
+});
+
+app.get("/collection/get", function (req, res) {
+  if (vizSession) {
+    res.send(
+      morphCore.Collection.condenseCollection(vizSession.sourceCollection)
+    );
+  } else res.send("collection hasn't been loaded yet");
 });
 
 export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import express from "express";
+import { app } from "./app";
+import http from "http";
+import kill from "kill-port";
 
-const app = express();
+const port = 3000;
+
+kill(port, "tcp").then(() => {
+  const server = http.createServer(app).listen(port, () => {
+    console.log(
+      `Started Morph dbms server. Listening at http://localhost:${port}`
+    );
+  });
+});
