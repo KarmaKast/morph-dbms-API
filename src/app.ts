510f6cc96bd1da5b0a6423fa14163f4ab0833988
sree chandan <sreechandan98@gmail.com>
2020-04-17 23:16:18 +0530

    improvements

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,11 @@ app.post("/collection/save", function (req, res) {
   res.send("Success");
 });
 
+app.post("/collection/clear", function (req, res) {
+  vizSession.clear();
+  res.send("Success");
+});
+
 app.post("/collection/load", function (req, res) {
   console.log(req.body);
   vizSession = morphViz.load(
@@ -91,15 +96,67 @@ app.get("/entity/get", function (req, res) {
 
 app.post("/entity/updateLabel", function (req, res) {
   if (vizSession) {
-    const entityID: string | undefined = req.params.entityID;
+    console.log(req.query, req.body);
+    const entityID: string | undefined = req.query.entityID as
+      | string
+      | undefined;
     const sourceEntity = entityID
       ? vizSession.sourceCollection.Entities.get(entityID)
       : undefined;
     if (entityID && sourceEntity) {
       sourceEntity.Label = req.body.Label;
       res.send("success");
-    }
-  } else res.status(404).send("collection hasn't been loaded yet or no entityID provided or entity with ID does not exist");
+    } else
+      res
+        .status(404)
+        .send("no entityID provided or entity with ID does not exist");
+  } else res.status(404).send("collection hasn't been loaded yet");
+});
+
+app.post("/entity/updateProps", function (req, res) {
+  if (vizSession) {
+    console.log(req.query, req.body);
+    const entityID: string | undefined = req.query.entityID as
+      | string
+      | undefined;
+    const vizEntity = entityID ? vizSession.getVizEntity(entityID) : undefined;
+    const body = JSON.parse(req.body.props);
+    console.log(body);
+    if (entityID && vizEntity) {
+      Object.keys(body).forEach((key) => {
+        if (vizEntity.Data) vizEntity.Data.set(key, body[key]);
+      });
+      res.send("success");
+    } else
+      res
+        .status(404)
+        .send("no entityID provided or entity with ID does not exist");
+  } else res.status(404).send("collection hasn't been loaded yet");
+});
+
+function sendAndSleep(response: Response, counter: number): void {
+  if (counter > 10) {
+    response.end();
+  } else {
+    response.write({ i: counter });
+    counter++;
+    setTimeout(function () {
+      sendAndSleep(response, counter);
+    }, 1000);
+  }
+}
+
+app.get("/stream", function (req, res) {
+  //when using text/plain it did not stream
+  //without charset=utf-8, it only worked in Chrome, not Firefox
+  res.writeHead(200, {
+    "Content-Type": "text/html; charset=utf-8",
+    "Transfer-Encoding": "chunked",
+  });
+  //res.setHeader("Transfer-Encoding", "chunked");
+
+  res.write("Thinking...");
+  sendAndSleep(res, 1);
 });
 
 export { app };
